5bd3a599644bce21c80d899644685d574e8d86ef
Thaynná Duarte <thaynnaduarte14@gmail.com>
2025-07-21 11:32:39 -0300

    CategoryResource

---
+++ src/main/java/io/github/thaynnaduarte/dscatalog/entites/Category.java
@@ -0,0 +1,52 @@
+package io.github.thaynnaduarte.dscatalog.entites;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private long id;
+    private String name;
+
+
+    public Category() {
+
+    }
+
+    public Category(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id == category.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
+}
