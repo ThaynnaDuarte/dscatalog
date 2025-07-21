9ee52c5d6c09e8c4d88936c356a5246fb86d5ab5
Thaynná Duarte <thaynnaduarte14@gmail.com>
2025-07-21 16:08:37 -0300

    Exceptions

--- src/main/java/io/github/thaynnaduarte/dscatalog/services/CategoryService.java
+++ src/main/java/io/github/thaynnaduarte/dscatalog/services/CategoryService.java
@@ -3,12 +3,14 @@ package io.github.thaynnaduarte.dscatalog.services;
 import io.github.thaynnaduarte.dscatalog.dto.CategoryDTO;
 import io.github.thaynnaduarte.dscatalog.entites.Category;
 import io.github.thaynnaduarte.dscatalog.repositories.CategoryRepository;
+import io.github.thaynnaduarte.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -21,5 +23,12 @@ public class CategoryService {
     public List<CategoryDTO> findAll() {
         List<Category> list = repository.findAll();
         return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Category not found"));
+        return new CategoryDTO(entity);
     }
 }
