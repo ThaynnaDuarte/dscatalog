bd0716ba8503da0668cc7d1d5bf1dd9224923a21
Thaynná Duarte <thaynnaduarte14@gmail.com>
2025-07-21 21:49:26 -0300

    Auditing data

--- src/main/java/io/github/thaynnaduarte/dscatalog/entites/Category.java
+++ src/main/java/io/github/thaynnaduarte/dscatalog/entites/Category.java
@@ -3,6 +3,7 @@ package io.github.thaynnaduarte.dscatalog.entites;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 
 @Entity
@@ -16,6 +17,11 @@ public class Category implements Serializable {
     private long id;
     private String name;
 
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
 
     public Category() {
     }
@@ -39,6 +45,24 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
     }
 
     @Override
